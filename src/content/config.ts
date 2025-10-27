5ae7ad72c246d149e3346b4f81b35b80dbe509ae
Sarai Marte <smartepujols@gmail.com>
2025-10-27 00:39:23 -0400

    102725 lizzy learns!!

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,4 @@
 import { defineCollection, z } from 'astro:content';
-const calc1Collection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-  }),
-});
 
 const calc2Collection = defineCollection({
   type: 'content',
@@ -21,122 +11,6 @@ const calc2Collection = defineCollection({
   }),
 });
 
-const maccCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-    chapter: z.string(),
-  }),
-});
-
-const statMethodsCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-  }),
-});
-
-
-const supplyChainCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-  }),
-});
-
-const writingForMediaCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-  }),
-});
-
-const compsciCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string(),
-    chapter: z.string(),
-  }),
-});
-
-const latinoWorkersCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-
-const fmanageCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-
-const misCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const dataStructuresCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const linearAlgCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-
 const discreteStructures1Collection = defineCollection({
   type: 'content',
   schema: z.object({
@@ -148,62 +22,6 @@ const discreteStructures1Collection = defineCollection({
   }),
 });
 
-
-const corporateFinanceCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-
-const operationsManagementCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const investmentAnalysisCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const computerArchitectureCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const cs336Collection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
 
 const aiCollection = defineCollection({
   type: 'content',
@@ -228,63 +46,10 @@ const discreteStructures2Collection = defineCollection({
 });
 
 
-
-const softMethCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-const cs344Collection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
-
-const IBCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    updatedDate:z.date(),
-    draft: z.boolean(),
-    author: z.string()
-  }),
-});
-
 export const collections = {
-  calc1: calc1Collection,
   calc2: calc2Collection,
-  macc: maccCollection,
-  statMethods: statMethodsCollection,
-  supplyChain:supplyChainCollection,
-  writingForMedia: writingForMediaCollection,
-  compsci:compsciCollection,
-  latinoWorkers: latinoWorkersCollection,
-  fmanage:fmanageCollection,
-  mis:misCollection,
-  dataStructures: dataStructuresCollection,
-  linearAlg: linearAlgCollection,
   discreteStructures1: discreteStructures1Collection,
-  corporateFinance: corporateFinanceCollection,
-  operationsManagement: operationsManagementCollection,
-  investmentAnalysis:investmentAnalysisCollection,
-  computerArchitecture: computerArchitectureCollection,
-  cs336:cs336Collection,
   ai: aiCollection,
   discreteStructures2:discreteStructures2Collection,
-  softwareMethodology: softMethCollection,
-  cs344:cs344Collection,
-  investmentBankingAnalysis:IBCollection
+
 };
